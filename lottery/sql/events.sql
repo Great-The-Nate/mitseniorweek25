58336a40c8e0dcc46b0d368ed400c9d391a913b6
Nathan Mustafa <natemustafanam@gmail.com>
2025-05-08 14:48:23 -0400

    Add descriptions to events

--- lottery/sql/events.sql
+++ lottery/sql/events.sql
@@ -9,7 +9,7 @@ INSERT INTO events
             NULL,
             NULL,
             false,
-            NULL
+            'Kicking off the senior week with a return to where it all began: elementary school. More specifically, elementary school field day. Come with your friends to join teams and play games like 3-legged-races, pie-eating competitions, water slides, etc.!'
         ),
         (
             'Pool Party',
@@ -19,17 +19,17 @@ INSERT INTO events
             NULL,
             NULL,
             false,
-            'Max 100 people in the pool at a time.'
+            'A pool party for the ages. Lights will be dim, floaties will be floating, Nintendo switch will be played, snacks will be served. We''ve all taken the swim test by now, hopefully, let''s celebrate together! Tickets for this event are free. Keep in mind there is a pool capacity of 100 people at a time, and a pool gallery capacity of 550 people at a time. Make sure to come the pool with your friends so you can get in together.'
         ), 
         (
             'Level99',
             '5/22/2025',
             '7:00 PM - 9:00 PM',
             NULL,
-            12.50,
+            15.00,
             200,
             true,
-            'Buses leave at 6:00 PM.'
+            'Come hang with friends while you solve mini-games, short escape rooms, and other challenges both physical and mental. Buses leave at 6:00 PM.'
         ),
         (
             'Red Sox Game',
@@ -39,17 +39,17 @@ INSERT INTO events
             15,
             300,
             true,
-            NULL
+            'Attend a baseball game in one of the most iconic ball parks in the entire world. It is a must see before you leave Boston.'
         ),
         (
             'Top Golf',
             '5/25/2025',
             '1:00 PM - 3:00 PM',
             NULL,
-            12.50,
+            15.00,
             300,
             true,
-            'Buses leave at 12:00 PM.'
+            'Swing into some friendly competition at Top Golf! Whether you''re a pro or picking up a club for the first time, it''s all about the good vibes and great company. Light snacks will be provided, but we suggest eating beforehand to keep your energy up through all those hole-in-one attempts! Buses leave at 12:00 PM.'
         ),
         (
             'Movie & Crafts Night',
@@ -59,7 +59,7 @@ INSERT INTO events
             NULL,
             NULL,
             false,
-            NULL
+            'Watch a movie under the stars as you enjoy the delicious popcorn that is freshly made on site. Tickets for this event are free.'
         ),
         (
             'Swapfest',
@@ -69,7 +69,7 @@ INSERT INTO events
             NULL,
             NULL,
             false,
-            NULL
+            'Are you looking to give away some stuff and maybe take some stuff from other graduating seniors? Like a minor major merch swap? Or a little athletic team shirt swap? Or some knick-knack exchanging? If so, pull up to this event where some of your fellow graduating seniors will be giving and taking things.'
         ),
         (
             'Senior Toast',
@@ -79,17 +79,17 @@ INSERT INTO events
             NULL,
             NULL,
             false,
-            NULL
+            'It''s time to say goodbye - after a long, beautiful, long four years, let us come together and toast to the class of 2025. We''ll have (non-alcoholic) sparkling cider for all, and we''ll snap a class photo to commemorate the end of Senior Week and senior year. Semi-formal attire.'
         ),
         (
             'Cafe Runaway',
             '5/27/2025',
             '10:00 AM - 1:00 PM',
             'Across Boston!',
-            NULL,
-            NULL,
-            false,
-            NULL
+            5,
+            150,
+            true,
+            'Run (or walk or bike or Uber) to your favorite cafes in the Boston area including Levain Bakery, Mamaleh''s, Formaggio, and more. Comes with three free treat coupons.'  
         ),
         (
             'Taza Chocolate Lab Tours',
@@ -99,7 +99,7 @@ INSERT INTO events
             5,
             100,
             true,
-            'Several 1 hr. tours throughout the day.'
+            'Join us at the factory for an Intro to Stone Ground Chocolate! We''ll have several 1 hr. tours of the Taza Chocolate Factory throughout the day. Signups for times will come out after ticketing.'
         ),
         (
             'Skyzone',
@@ -109,7 +109,7 @@ INSERT INTO events
             12.50,
             250,
             true,
-            'Buses leave at 5:00 PM.'
+            'Jump into a night of high-energy fun at SkyZone! From open jump areas to trampoline dodgeball, it''s the perfect chance to let loose and enjoy some active time with friends. Pizza will be provided, but we recommend grabbing a bite beforehand. Buses leave at 5:00 PM.'
 
         ),
         (
@@ -120,7 +120,7 @@ INSERT INTO events
             15,
             300,
             true,
-            '21+ only. One free drink included.'
+            'Join us for a night of games, food, and drinks at a Fenway classic - the Cheeky Monkey. If you''ve never been, you''re in for a treat, surrounded by your classmates and a bunch of painted monkey heads. 21+ only. One free drink included.'
         ),
         (
             'F1 Arcade',
@@ -130,7 +130,7 @@ INSERT INTO events
             15,
             200,
             true,
-            ''
+            'Get ready to channel your inner speedster at F1 Arcade! Race your friends in high-speed simulators and see who takes the podium. Limited food items like sliders and flatbread will be available, but we recommend having a light snack beforehand so you''re fueled up and ready to race!'
         ),
         (
             'Skydiving Day 1',
@@ -140,7 +140,7 @@ INSERT INTO events
             165,
             100,
             true,
-            'Buses leave at 4:00 AM. Expect to be gone all day. Overflow space filled on 7:00 AM buses on a first-come first-serve basis. Please note that we will be distributing all skydiving tickets in the first round to accommodate vendor timelines.'
+            'Please see information here: https://engage.mit.edu/mitseniorweek2025/rsvp_boot?id=916023. Note that we will be distributing all skydiving tickets in the first round to accommodate vendor timelines.'
         ),
         (
             'Skydiving Day 2',
@@ -150,7 +150,7 @@ INSERT INTO events
             165,
             100,
             true,
-            'Buses leave at 4:00 AM. Expect to be gone all day. Overflow space filled on 7:00 AM buses on a first-come first-serve basis. Please note that we will be distributing all skydiving tickets in the first round to accommodate vendor timelines.'
+            'Please see information here: https://engage.mit.edu/mitseniorweek2025/rsvp_boot?id=916023. Note that we will be distributing all skydiving tickets in the first round to accommodate vendor timelines.'
         ),
         (
             'Skydiving Day 3',
@@ -160,7 +160,7 @@ INSERT INTO events
             165,
             100,
             true,
-            'Buses leave at 4:00 AM. Expect to be gone all day. Overflow space filled on 7:00 AM buses on a first-come first-serve basis. Please note that we will be distributing all skydiving tickets in the first round to accommodate vendor timelines. Also note that Senior Ball is on this day.'
+            'Please see information here: https://engage.mit.edu/mitseniorweek2025/rsvp_boot?id=916023. Note that we will be distributing all skydiving tickets in the first round to accommodate vendor timelines. Also note that Senior Ball is on this day.'
         ),
         (
             'Skydiving Day 4',
@@ -170,7 +170,7 @@ INSERT INTO events
             165,
             100,
             true,
-            'Buses leave at 4:00 AM. Expect to be gone all day. Overflow space filled on 7:00 AM buses on a first-come first-serve basis. Please note that we will be distributing all skydiving tickets in the first round to accommodate vendor timelines.'
+            'Please see information here: https://engage.mit.edu/mitseniorweek2025/rsvp_boot?id=916023. Note that we will be distributing all skydiving tickets in the first round to accommodate vendor timelines.'
         ),
         (
             'Boston Scavenger Hunt',
@@ -180,7 +180,7 @@ INSERT INTO events
             NULL,
             NULL,
             false,
-            NULL
+            'It''s a scavenger hunt of things around MIT/Boston. You can work in teams to try to hit all of the destinations/tasks fastest. The top prize is the Senior Week Nintendo Switch with Mario Kart 😎 and the four runner ups get a cake 🍰!'
         ),
         (
             'Newbury Discount Codes',
